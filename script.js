8760e7e002511175b3bfd3b382326d712226c090
Justin Chin <justinmichaelchin@gmail.com>
2016-03-01 12:21:59 -0800

    more styling

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ function getGeoLocation() {
   var output = document.getElementById('start');
 
   if (!navigator.geoloction) {
-    output.innerHTML = '<p>Your browser does not support geolocation</p>';
+    output.innerHTML = 'Your browser does not support geolocation';
   }
 
-  function success(pos) {
+  function success(position) {
     // cache lat and long coords
-    var latitude  = pos.coords.latitude;
-    var longitude = pos.coords.longitude;
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
     console.log('latitude: ', latitude);
     console.log('longitude: ', longitude);
   }
 
   function error() {
-    alert('Unable to determine your location');
+    output.style['background-color'] = '#f75e5e';
+    output.innerHTML = 'Unable to determine your location';
+
   }
 
   output.innerHTML = 'Tracking...';
-  output.className += ' loading';
   navigator.geolocation.getCurrentPosition(success, error);
 }
 
