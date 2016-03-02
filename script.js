3deed175d795f719a534e9806de77b34f7dc34fd
jmchin <justinmichaelchin@gmail.com>
2016-03-01 20:42:54 -0800

    more styling, began openweather API integration

--- script.js
+++ script.js
@@ -9,8 +9,17 @@ function getGeoLocation() {
     // cache lat and long coords
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
-    console.log('latitude: ', latitude);
-    console.log('longitude: ', longitude);
+
+    // IIFE that references coordinates for API call
+    ( function getLocalWeather() {
+      var URL = 'http://api.openweathermap.org/data/2.5/weather?lat=%lat%&lon=%long%&APPID=21f42111c89049ac9a71560c5fe1d747';
+          URL = URL.replace('%lat%', latitude);
+          URL = URL.replace('%long%', longitude);
+
+      // TODO: ajax request for api JSON
+    
+    } )();
+
   }
 
   function error() {
